6c19cd47872a27e3b0f4bf69212f626909478cf9
Brian <wamwangimathenge@gmail.com>
2023-01-15 19:45:51 +0300

    Parent Component

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,21 +1,19 @@
-import React, { useState } from "react";
+import React from "react";
 
-function Form() {
-  const [firstName, setFirstName] = useState("John");
-  const [lastName, setLastName] = useState("Henry");
-
-  function handleFirstNameChange(event) {
-    setFirstName(event.target.value);
-  }
-
-  function handleLastNameChange(event) {
-    setLastName(event.target.value);
-  }
+function Form(props) {
 
   return (
     <form>
-      <input type="text" onChange={handleFirstNameChange} value={firstName} />
-      <input type="text" onChange={handleLastNameChange} value={lastName} />
+      <input 
+        type="text" 
+        onChange={props.handleFirstNameChange} 
+        value={props.firstName} 
+      />
+      <input 
+        type="text" 
+        onChange={props.handleLastNameChange} 
+        value={props.lastName} 
+      />
       <button type="submit">Submit</button>
     </form>
   );
